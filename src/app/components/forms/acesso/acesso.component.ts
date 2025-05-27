f7e96a054c77f50d4aefa8d109eef386844dff58
emily <emilyhiha12@gmail.com>
2025-05-26 21:11:33 -0300

    Commit inicial criando projeto base vazio

---
+++ src/app/components/forms/acesso/acesso.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-acesso',
+  imports: [],
+  templateUrl: './acesso.component.html',
+  styleUrl: './acesso.component.scss'
+})
+export class AcessoComponent {
+
+}
---
+++ src/app/pages/movimentacoes/movimentacoes.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movimentacoes',
+  imports: [],
+  templateUrl: './movimentacoes.component.html',
+  styleUrl: './movimentacoes.component.scss'
+})
+export class MovimentacoesComponent {
+
+}
