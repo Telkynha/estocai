472e563f60ae2bcba540b8df5499e8e744ebc1d4
emily <emilyhiha12@gmail.com>
2025-05-26 21:41:47 -0300

    Criando mapeamento das paginas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { EstoqueComponent } from './pages/estoque/estoque.component';
+import { MovimentacoesComponent } from './pages/movimentacoes/movimentacoes.component';
+import { EstatisticasComponent } from './pages/estatisticas/estatisticas.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'estoque', component: EstoqueComponent },
+    { path: 'movimentacoes', component: MovimentacoesComponent },
+    { path: 'estatisticas', component: EstatisticasComponent },
+];
