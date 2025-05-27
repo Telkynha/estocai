9b06c76b882af6df58f1edce9b86a94137d373a0
emily <emilyhiha12@gmail.com>
2025-05-26 21:46:39 -0300

    Navegação funcionando

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,15 @@ import { HomeComponent } from './pages/home/home.component';
 import { EstoqueComponent } from './pages/estoque/estoque.component';
 import { MovimentacoesComponent } from './pages/movimentacoes/movimentacoes.component';
 import { EstatisticasComponent } from './pages/estatisticas/estatisticas.component';
+import { ConfigComponent } from './components/forms/config/config.component';
+import { AcessoComponent } from './components/forms/acesso/acesso.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'estoque', component: EstoqueComponent },
     { path: 'movimentacoes', component: MovimentacoesComponent },
     { path: 'estatisticas', component: EstatisticasComponent },
+    { path: 'config', component: ConfigComponent },
+    { path: 'acesso', component: AcessoComponent },
+    { path: '**', redirectTo: '' }
 ];
