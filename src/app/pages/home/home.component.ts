b8836dc03f6eb34776eedbc77360c9893ff8f3fa
emily <emilyhiha12@gmail.com>
2025-05-28 17:32:07 -0300

    Ajustando layout e estilo de componentes e importanto icone

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-home',
-  imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss'],
+  standalone: true,
+  imports: [CommonModule, MatCardModule, MatIconModule]
 })
 export class HomeComponent {
-
+  cards = [
+    {
+      title: 'Total em Estoque',
+      value: '1.234',
+      icon: 'inventory_2',
+      trend: '+5%'
+    },
+    {
+      title: 'Movimentações Hoje',
+      value: '47',
+      icon: 'swap_horiz',
+      trend: '+12%'
+    },
+    {
+      title: 'Produtos Críticos',
+      value: '8',
+      icon: 'warning',
+      trend: '-2%'
+    },
+    {
+      title: 'Valor Total',
+      value: 'R$ 45.678',
+      icon: 'payments',
+      trend: '+8%'
+    }
+  ];
 }
