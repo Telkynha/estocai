62e45692a505b29785df08b11dda10a6a5036ade
emily <emilyhiha12@gmail.com>
2025-08-11 18:39:04 -0300

    Populando a pagina de login e cadastro

--- src/app/components/forms/acesso/acesso.component.ts
+++ src/app/components/forms/acesso/acesso.component.ts
@@ -1,23 +1,58 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MatDialogModule } from '@angular/material/dialog';
-import { MatListModule } from '@angular/material/list';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-acesso',
+  standalone: true,
   imports: [
     CommonModule,
     MatDialogModule,
-    MatListModule,
+    MatTabsModule,
+    MatFormFieldModule,
+    MatInputModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    ReactiveFormsModule
   ],
-  standalone: true,
   templateUrl: './acesso.component.html',
   styleUrl: './acesso.component.scss'
 })
 export class AcessoComponent {
+  loginForm: FormGroup;
+  registerForm: FormGroup;
+  hidePassword = true;
 
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+
+    this.registerForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onLogin() {
+    if (this.loginForm.valid) {
+      console.log('Login:', this.loginForm.value);
+      // Implement your login logic here
+    }
+  }
+
+  onRegister() {
+    if (this.registerForm.valid) {
+      console.log('Register:', this.registerForm.value);
+      // Implement your registration logic here
+    }
+  }
 }
